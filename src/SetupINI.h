a58ed3c5b41b4dd89aa64060a84a360b033263da
KrebKrebou <130295160+KrebKrebou@users.noreply.github.com>
2023-07-30 17:15:23 +0300

    Minor INI comment edit

--- src/SetupINI.h
+++ src/SetupINI.h
@@ -8,12 +8,15 @@ void SetupINI() {
     ini.SetUnicode();
     ini.LoadFile("Data\\SKSE\\Plugins\\LockBashSKSE.ini");
 
-    const char* formulaCom = "# 0 = Easy | 1 = Requiem-like (Recommended)";
-    const char* notificationCom = "# Turn HUD notifications on/off";
+    const char* formulaCom = "# 1 = Requiem-like (Recommended)| 2 = Easy ";
+    const char* notificationCom = "# false = HUD notifications off | true = HUD notifications on";
 
     if (ini.IsEmpty()) {
         ini.SetLongValue("Gameplay", "Formula", 1, formulaCom, false, false);
         ini.SetBoolValue("Misc", "Notifications", false, notificationCom, false);
         ini.SaveFile("Data\\SKSE\\Plugins\\LockBashSKSE.ini");
+    }
+    else if (!ini.IsEmpty()) {
+        //do nothing
     }
 }
