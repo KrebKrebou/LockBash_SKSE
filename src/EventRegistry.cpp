2ba8c2e840984b2bd6421862ab4e2c8db68a1e57
KrebKrebou <130295160+KrebKrebou@users.noreply.github.com>
2023-08-07 22:36:43 +0300

    Event Registry Organized
    -commented out unused variables

--- src/EventRegistry.cpp
+++ src/EventRegistry.cpp
@@ -2,56 +2,49 @@
 
 void RegisterForEvent_Hit() {
     struct EventSink : public RE::BSTEventSink<RE::TESHitEvent> {
-        RE::BSEventNotifyControl ProcessEvent(const RE::TESHitEvent* event, RE::BSTEventSource<RE::TESHitEvent>* source) {
+        RE::BSEventNotifyControl ProcessEvent(const RE::TESHitEvent* event, RE::BSTEventSource<RE::TESHitEvent>* /*source*/) {
 
             if (event->cause.get() != nullptr) {
                 if (event->cause.get()->Is(RE::FormType::ActorCharacter)) {
-                    // actor
-                    auto actorREFptr = event->cause;
-                    auto actorREF = actorREFptr->AsReference();
-                    auto actorName = actorREFptr->GetBaseObject()->GetName();
-                    auto actorACT = actorREFptr->As<RE::Actor>();
+                    if (event->target->GetBaseObject()->Is(RE::FormType::Door) || event->target->GetBaseObject()->Is(RE::FormType::Container)) {
 
-                    auto Health = actorACT->AsActorValueOwner()->GetActorValue(RE::ActorValue::kHealth);
-                    auto Stamina = actorACT->AsActorValueOwner()->GetActorValue(RE::ActorValue::kHealth);
-                    auto Power = Health + Stamina;
+                        /*target*/
+                        auto targetREFptr = event->target;
+                        //auto targetREF = targetREFptr->AsReference();
+                        //auto targetName = targetREFptr->GetBaseObject()->GetName();
+                        //auto targetFormType = targetREFptr->GetBaseObject()->GetFormType();
 
-                    if (ini.GetLongValue("Gameplay", "Formula", 1) == 1) {
-                        Power = 2 * Health + Stamina;
-                    }
+                        /*actor*/
+                        auto actorREFptr = event->cause;
+                        auto actorACT = actorREFptr->As<RE::Actor>();
+                        //auto actorREF = actorREFptr->AsReference();
+                        //auto actorName = actorREFptr->GetBaseObject()->GetName();
 
-                    auto weap = RE::TESForm::LookupByID(event->source);
-                    auto weapWEAP = weap->As<RE::TESObjectWEAP>();
-                    auto weapFormType = weap->GetFormType();
-                    auto weapState = actorACT->AsActorState()->GetWeaponState();
-                    auto attackState = actorACT->AsActorState()->GetAttackState();
-                    auto proj = event->projectile;
+                        /*actorvalues*/
+                        auto Health = actorACT->AsActorValueOwner()->GetActorValue(RE::ActorValue::kHealth);
+                        auto Stamina = actorACT->AsActorValueOwner()->GetBaseActorValue(RE::ActorValue::kHealth);
+                        auto Power = Health + Stamina;
+                        if (ini.GetLongValue("Gameplay", "Formula", 1) == 1) {Power = 2 * Health + Stamina;}
 
-                    auto kBashAttack = RE::AttackData::AttackFlag::kBashAttack;
-                    auto kChargeAttack = RE::AttackData::AttackFlag::kChargeAttack;
-                    auto kContinuousAttack = RE::AttackData::AttackFlag::kContinuousAttack;
-                    auto kIgnoreWeapon = RE::AttackData::AttackFlag::kIgnoreWeapon;
-                    auto kNone = RE::AttackData::AttackFlag::kNone;
-                    auto kOverrideData = RE::AttackData::AttackFlag::kOverrideData;
-                    auto kPowerAttack = RE::AttackData::AttackFlag::kPowerAttack;
-                    auto kRotatingAttack = RE::AttackData::AttackFlag::kRotatingAttack;
+                        /*weapons*/
+                        auto weap = RE::TESForm::LookupByID(event->source);
+                        auto weapFormType = weap->GetFormType();
+                        auto proj = event->projectile;
+                        //auto weapWEAP = weap->As<RE::TESObjectWEAP>();
+                        //auto weapState = actorACT->AsActorState()->GetWeaponState();
+                        //auto attackState = actorACT->AsActorState()->GetAttackState();
 
-                    //std::vector<RE::AttackData::AttackFlag> attFlagVec = { kBashAttack, kChargeAttack, kContinuousAttack, kIgnoreWeapon, kNone, kOverrideData, kPowerAttack, kRotatingAttack };
-                    //RE::AttackData::AttackFlag attFlagReturn;
-                    //for (RE::AttackData::AttackFlag val : attFlagVec) {
-                    //    attFlagReturn = val;
-                    //}
+                        /*attacktypes*/
+                        auto kBashAttack = RE::AttackData::AttackFlag::kBashAttack;
+                        auto kChargeAttack = RE::AttackData::AttackFlag::kChargeAttack;
+                        auto kContinuousAttack = RE::AttackData::AttackFlag::kContinuousAttack;
+                        auto kIgnoreWeapon = RE::AttackData::AttackFlag::kIgnoreWeapon;
+                        auto kNone = RE::AttackData::AttackFlag::kNone;
+                        auto kOverrideData = RE::AttackData::AttackFlag::kOverrideData;
+                        auto kPowerAttack = RE::AttackData::AttackFlag::kPowerAttack;
+                        auto kRotatingAttack = RE::AttackData::AttackFlag::kRotatingAttack;
 
-                    // target
-                    auto targetREFptr = event->target;
-                    auto targetREF = targetREFptr->AsReference();
-                    auto targetName = targetREFptr->GetBaseObject()->GetName();
-                    auto targetFormType = targetREFptr->GetBaseObject()->GetFormType();
-
-
-                    // Check for form type
-                    if (targetFormType == RE::FormType::Door || targetFormType == RE::FormType::Container) {
-                        // Check for weapon
+                        /*Check for weapon*/
                         if (weapFormType == RE::FormType::Weapon && !proj) {
                             auto attackData = actorACT->GetActorRuntimeData().currentProcess->high->attackData->data.flags;
                             if (attackData.none(kBashAttack, kChargeAttack, kContinuousAttack, kIgnoreWeapon, kNone, kOverrideData, kPowerAttack, kRotatingAttack)) {
